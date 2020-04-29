19203e12205c178c235d04dd335b4cd32776f3d8
paulwstorm <anapaulorocket@gmail.com>
2020-04-29 15:20:16 -0400

    added wireframe doc

--- server/dbmanagement.js
+++ server/dbmanagement.js
@@ -18,7 +18,14 @@ con.query("DROP TABLE users", function (err, result) {
   console.log("Table deleted")
 })
 
-var users = "CREATE TABLE users (id VARCHAR(255) PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), firstName CHAR(30), lastName CHAR(30), profile_pic VARCHAR(255), date_created DATETIME)";
+var users = "CREATE TABLE users ( \
+  id VARCHAR(255) PRIMARY KEY, \
+  username VARCHAR(255), \
+  password VARCHAR(255), \
+  firstName CHAR(30), \
+  lastName CHAR(30), \
+  profilePic VARCHAR(255), \
+  dateCreated DATETIME)";
 con.query(users, function (err, result) {
   if (err) throw err;
   console.log("Tables created");
